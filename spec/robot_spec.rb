9ae936c371c4d51c46c463ce08ea7a6fb380cda7
Salman Sohail <salmansohail@agilis-lab.com>
2020-08-23 00:40:27 +1000

    Basic functionality of robot class and specs are added

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -19,29 +19,16 @@ describe Robot do
     it { expect(robot.valid_command?('SOMETHING')).to eq(false) }
 
     context 'when PLACE command is invalid' do
-      it 'will not validate if the length of command is not equal to 4' do
-        expect(robot.valid_command?('PLACE')).to eq(false)
-        expect(robot.valid_command?('PLACE A, B')).to eq(false)
-        expect(robot.valid_command?('PLACE A, B, F, G')).to eq(false)
-      end
-
-      it 'will not validate if PLACE is not the first parameter' do
-        expect(robot.valid_command?('A, B, PLACE, Y')).to eq(false)
-        expect(robot.valid_command?('A, PLACE,B, Y')).to eq(false)
-        expect(robot.valid_command?('A, B, PLACE, Y')).to eq(false)
-      end
-
-      it 'will not validate if PLACE command coordinates are not integers' do
-        expect(robot.valid_command?('PLACE A, B, Y')).to eq(false)
-        expect(robot.valid_command?('PLACE A, 0, Y')).to eq(false)
-        expect(robot.valid_command?('PLACE 0, B, Y')).to eq(false)
-        expect(robot.valid_command?('PLACE 90, B, Y')).to eq(false)
-      end
-
-      it 'will not validate if the PLACE command has invalid FACING' do
-        expect(robot.valid_command?('PLACE 0, 0, Y')).to eq(false)
-      end
+      it { expect(robot.valid_command?('PLACE')).to eq(false) }
+      it { expect(robot.valid_command?('PLACE A, B')).to eq(false) }
+      it { expect(robot.valid_command?('PLACE A, B, F, G')).to eq(false) }
+      it { expect(robot.valid_command?('A, B, PLACE, Y')).to eq(false) }
+      it { expect(robot.valid_command?('A, PLACE,B, Y')).to eq(false) }
+      it { expect(robot.valid_command?('A, B, PLACE, Y')).to eq(false) }
+      it { expect(robot.valid_command?('PLACE A, B, Y')).to eq(false) }
+      it { expect(robot.valid_command?('PLACE A, 0, Y')).to eq(false) }
+      it { expect(robot.valid_command?('PLACE 0, B, Y')).to eq(false) }
+      it { expect(robot.valid_command?('PLACE 0, 0, Y')).to eq(false) }
     end
   end
-  
 end
