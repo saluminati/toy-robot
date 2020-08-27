b4c55b51848a31c2dc595763cc8832c8597ef044
Salman Sohail <salmansohail@agilis-lab.com>
2020-08-27 11:21:49 +1000

    improved namespacing of the project and created much more organized folder structure, categorized errors for robot, table and commands

--- constants/application_constants.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-DIRECTIONS = %w[NORTH EAST SOUTH WEST].freeze
-COMMANDS = %w[MOVE LEFT RIGHT REPORT PLACE].freeze
-INVALID_COMMAND = 'INVALID_COMMAND'.freeze
-PLACE = COMMANDS.last.freeze
-PLACE_COMMAND_PATTERN = Regexp.new /#{PLACE}\s*(\d+)\s*,\s*(\d+)\s*,\s*(#{DIRECTIONS.join('|')})\s*$/
